a24250e1781438d754a3c975a48a8d8891445f50
maxim <st119024@student.spbu.ru>
2024-08-21 12:01:15 +0500

    add cast crew model

--- films/models.py
+++ films/models.py
@@ -28,6 +28,10 @@ GENRES = [
     ('WST', 'Western'),
 ]
 
+PROFESSIONS = [
+    ('DIR', 'Director'),
+    ('ACT', 'Actor'),
+]
 
 class Genre(models.Model):
     name = models.CharField(max_length=30, choices=GENRES)
@@ -41,6 +45,25 @@ class Genre(models.Model):
         ordering = ["name"]
 
 
+class CastCrew(models.Model):
+    id = models.BigAutoField(primary_key=True, auto_created=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateTimeField(blank=True, null=True)
+    profession = models.CharField(max_length=30, blank=True, null=True, choices=PROFESSIONS)
+    
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def get_absolute_url(self):
+        return reverse('cast_crew_detail', args=[str(self.id)]) # /myapplication/mymodelname/2
+
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+
 class Film(models.Model):
     id = models.BigAutoField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=100, unique=True)
@@ -50,9 +73,8 @@ class Film(models.Model):
     budget = models.BigIntegerField(blank=True, null=True)
     box_office = models.BigIntegerField(blank=True, null=True)
     cover = models.ImageField(blank=True, null=True)
-    genres = models.ManyToManyField('Genre', blank=True) # models.CharField(max_length=30, choices=GENRES)
-    # director = models.ForeignKey('CastCrew', on_delete=models.SET_NULL, blank=True, null=True)
-    # actors = models.ManyToManyField('CastCrew', blank=True, null=True)
+    genres = models.ManyToManyField('Genre', blank=True)
+    cast_crew = models.ManyToManyField('CastCrew', blank=True)
     
 
     def __str__(self):
