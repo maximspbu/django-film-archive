6c9655bde2ab084ba4111974b1b7080daf2019b0
maxim <st119024@student.spbu.ru>
2024-08-31 12:54:31 +0500

    add tests

---
+++ films/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from films.models import Film, Profession
+from datetime import datetime, timedelta
+
+class FilmModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Film.objects.create(
+            name='FilmModelTest',
+            storyline='Some text.',
+            release_date=datetime.today().strftime('%Y-%m-%d'),
+            running_time=timedelta(hours=2, minutes=30),
+            budget=100000000,
+            box_office=523221337,
+        )
+
+    def test_check_release_date(self):
+        film = Film.objects.get(pk=1)
+        self.assertLessEqual(film.release_date.strftime('%Y-%m-%d'), datetime.today().strftime('%Y-%m-%d'))
