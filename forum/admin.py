74becc19a095539e2f9eac8602b56455ca22653b
maxim <st119024@student.spbu.ru>
2024-09-13 14:04:48 +0300

    add message, topic, reviews, custom user and admin models

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'registration_date',
+        'is_active',
+        'is_stuff',
+        'is_superuser',
+    )
+
+@admin.register(models.Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'reply',
+        'created_at',
+    )
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'film',
+        'created_at',
+    )
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'created_at',
+        'updated_at',
+    )
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.urls import reverse
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from datetime import date, datetime
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Invalid email!")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, extra_fields=extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_stuff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(self, email, password, extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_stuff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(self, email, password, extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=320, unique=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.TextField(max_length=600, blank=True, null=True)
+    registration_date = models.DateField(default=date.today)
+    avatar = models.ImageField(upload_to='images/avatars', blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_stuff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def get_absolute_url(self):
+        return reverse('user_detail', args=[str(self.id)])
+
+
+class BaseContent(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return str(self.id)
+    
+    def get_absolute_url(self):
+        return reverse(f'{self._meta.model_name.lower()}_detail', args=[str(self.id)])
+
+    class Meta:
+        abstract = True
+
+
+# class FileContent(models.Model):
+#     content = models.ForeignKey(BaseContent, on_delete=models.CASCADE)
+#     file = models.FileField(upload_to='media')
+
+
+class Message(BaseContent):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    related_object = GenericForeignKey('content_type', 'object_id')
+    reply = models.ForeignKey("Message", blank=True, null=True, on_delete=models.CASCADE)
+
+
+class Review(BaseContent):
+    film = models.ForeignKey("films.Film", on_delete=models.CASCADE)
+
+
+class Topic(BaseContent):
+    updated_at = models.DateTimeField(default=datetime.now)
