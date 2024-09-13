f3fbe8a13c925d02eb26d655c8d792c743af2d77
maxim <st119024@student.spbu.ru>
2024-09-13 14:05:29 +0300

    add views and templates, urls

---
+++ forum/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+
+urlpatterns = [
+    path('', views.Home.as_view(), name='forum_home'),
+    path('users/<pk>/', views.UserDetailView.as_view(), name='user_detail'),
+    #path('users/<pk:int>/chat/', views.ChatDetailView.as_view(), name='user_detail'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
+    path('topics/<pk>/', views.TopicDetailView.as_view(), name='topic_detail'),
+    path('topics/', views.TopicListView.as_view(), name='topic_list'),
+    path('reviews/<pk>/', views.ReviewDetailView.as_view(), name='review_detail'),
+    path('reviews/', views.ReviewListView.as_view(), name='review_list'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
+from .models import CustomUser, Topic, Review
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'forum/index.html'
+
+
+class UserDetailView(DetailView):
+    model = CustomUser
+    template = 'forum/user_detail.html'
+
+
+class UserListView(ListView):
+    model = CustomUser
+    template = 'forum/user_list.html'
+
+
+class TopicDetailView(DetailView):
+    model = Topic
+    template = 'forum/topic_detail.html'
+
+
+class TopicListView(ListView):
+    model = Topic
+    template = 'forum/topic_list.html'
+
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template = 'forum/review_detail.html'
+
+
+class ReviewListView(ListView):
+    model = Review
+    template = 'forum/review_list.html'
